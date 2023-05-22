264b74901677a653eaf36718fcec3a5c6a3c24a8
Jun Kim <kssjej6914@gmail.com>
2023-05-22 16:14:03 +0900

    viewBinding 추가

--- app/src/main/java/com/example/kooglemaps/MainActivity.kt
+++ app/src/main/java/com/example/kooglemaps/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.kooglemaps
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.kooglemaps.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding= ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
     }
 }
